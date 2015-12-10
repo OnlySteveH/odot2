b47b436ec0e5aec1333efb38d9bc73596d456616
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2015-12-10 10:55:47 +0000

    Cleaned up edit page and made flash messages consistent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  add_flash_types :success, :failure
+  add_flash_types :success, :error
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActiveSupport::MessageVerifier::InvalidSignature, with: :render_error
 
@@ -43,7 +43,7 @@ class ApplicationController < ActionController::Base
     if current_user
       true
     else
-      redirect_to new_user_session_path, notice: "You must be logged in to access that page."
+      redirect_to new_user_session_path, error: "You must be logged in to access that page."
     end
   end
 end
